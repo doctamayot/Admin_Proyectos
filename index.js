15750f5cd55bdd2bdd43f29c41f2d1061fe7c09d
Hugo Tamayo <doctamayot@hotmail.com>
2023-06-20 08:41:46 -0500

    config mongoose mongo conexion

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require('express')
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
 
 const app = express()
 
-app.listen(4000, ()=>{ 
-    console.log("Servidor corriendo en puerto 4000")
+dotenv.config()
+
+conectarDB()
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, ()=>{ 
+    console.log(`Servidor corriendo en puerto ${PORT}`)
 })
